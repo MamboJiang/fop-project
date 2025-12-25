5430a7f274b6c359cecf3ecf1f21518f0dff6a3e
ILoveGraphics <149312421+ILoveGraphics@users.noreply.github.com>
2025-12-25 21:32:59 +0100

    Floor -> Path

--- core/src/de/tum/cit/fop/maze/GameObj/Path.java
+++ core/src/de/tum/cit/fop/maze/GameObj/Path.java
@@ -2,8 +2,8 @@ package de.tum.cit.fop.maze.GameObj;
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
-public class Floor extends GameObject {
-    public Floor(float x, float y, float width, float height, TextureRegion textureRegion) {
+public class Path extends GameObject {
+    public Path(float x, float y, float width, float height, TextureRegion textureRegion) {
         super(x, y, width, height, textureRegion);
     }
 }
